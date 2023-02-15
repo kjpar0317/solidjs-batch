c0b9d165f29375507204fd9aedb200484e1b1b2d
kjpar0317 <kjpar_kr@yahoo.co.kr>
2023-02-15 17:34:33 +0900

    Update index.ts

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,2 +1,39 @@
-export * from "./useLayout";
-export * from "./useAuth";
+import { createContext, JSXElement, useContext } from "solid-js";
+import { createStore } from "solid-js/store";
+import createLayout from "./createLayout";
+import createAuth from "./createAuth";
+
+const StoreContext = createContext();
+
+export function Provider(props: { children : JSXElement}) {
+    let layouts: any, auths: any;
+
+    // const router = createRouteHandler("");
+    const [state, setState] = createStore({
+        get layouts() {
+            return layouts;
+        },
+        get auths() {
+            return auths;
+        }
+    });
+
+    const actions = {};
+
+    const store = [
+        state, actions
+    ];
+
+    layouts = createLayout(state, setState, actions);
+    auths = createAuth(state, setState, actions);
+
+  return (
+        <StoreContext.Provider value={store}>
+            {props.children}
+        </StoreContext.Provider>
+  );
+}
+
+export function useStore() {
+    return useContext(StoreContext);
+};
