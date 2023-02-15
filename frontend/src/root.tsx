f15cf15db015df281b697f6c9474363980dc170b
kjpar0317 <kjpar_kr@yahoo.co.kr>
2023-02-15 17:33:55 +0900

    Update root.tsx

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -12,26 +12,34 @@ import {
   Title,
 } from "solid-start";
 import ErrorBoundartFallback from "~/components/layouts/error/ErrorBoundaryFallback";
+import { Provider, useStore } from "~/store";
+
 import "./root.css";
 
 export default function Root() {
+  function reset() {
+    console.log("aaa");
+  }
+
   return (
-    <Html lang="en">
-      <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
-        <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Body class="w-full h-full">
-        <Suspense>
-          <ErrorBoundary fallback={ErrorBoundartFallback}>
-            <Routes>
-              <FileRoutes />
-            </Routes>
-          </ErrorBoundary>
-        </Suspense>
-        <Scripts />
-      </Body>
-    </Html>
+    <Provider>
+      <Html lang="en">
+        <Head>
+          <Title>SolidStart - With TailwindCSS</Title>
+          <Meta charset="utf-8" />
+          <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Body class="w-full h-full">
+            <Suspense>
+              <ErrorBoundary fallback={(e) => ErrorBoundartFallback({ err: e, reset: reset })}>
+                <Routes>
+                  <FileRoutes />
+                </Routes>
+              </ErrorBoundary>
+            </Suspense>
+          <Scripts />
+        </Body>
+      </Html>
+    </Provider>
   );
 }
