3ac980aebf51fbf5c29e3b31785311f553eb6aee
kjpar0317 <kjpar0317@gmail.com>
2023-02-19 00:03:23 +0900

    minor 패치

--- frontend/src/store/index.tsx
+++ frontend/src/store/index.tsx
@@ -1,39 +1,38 @@
-import { createContext, JSXElement, useContext } from "solid-js";
+import type { JSXElement } from "solid-js";
+import { createContext, useContext } from "solid-js";
 import { createStore } from "solid-js/store";
 import createLayout from "./createLayout";
 import createAuth from "./createAuth";
 
 const StoreContext = createContext();
 
-export function Provider(props: { children : JSXElement}) {
-    let layouts: any, auths: any;
+export function Provider(props: { children: JSXElement }) {
+  let layouts: any, auths: any;
 
-    // const router = createRouteHandler("");
-    const [state, setState] = createStore({
-        get layouts() {
-            return layouts;
-        },
-        get auths() {
-            return auths;
-        }
-    });
+  // const router = createRouteHandler("");
+  const [state, setState] = createStore({
+    get layouts() {
+      return layouts;
+    },
+    get auths() {
+      return auths;
+    },
+  });
 
-    const actions = {};
+  const actions = {};
 
-    const store = [
-        state, actions
-    ];
+  const store = [state, actions];
 
-    layouts = createLayout(state, setState, actions);
-    auths = createAuth(state, setState, actions);
+  layouts = createLayout(state, setState, actions);
+  auths = createAuth(state, setState, actions);
 
   return (
-        <StoreContext.Provider value={store}>
-            {props.children}
-        </StoreContext.Provider>
+    <StoreContext.Provider value={store}>
+      {props.children}
+    </StoreContext.Provider>
   );
 }
 
 export function useStore() {
-    return useContext(StoreContext);
-};
+  return useContext(StoreContext);
+}
