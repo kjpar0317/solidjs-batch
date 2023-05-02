1088146095d4efcaec1ca7e1fce248c48ad6bcf3
kjpar0317 <kjpar_kr@yahoo.co.kr>
2023-05-02 17:24:24 +0900

    Update axios-utils.ts

--- frontend/src/utils/axios-utils.ts
+++ frontend/src/utils/axios-utils.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios, { AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from "axios";
 import toast from "solid-toast";
 
-const axiosUtils = axios.create({
+const axiosUtils: AxiosInstance = axios.create({
   baseURL: "",
   // baseURL: `${process.env.REACT_APP_BASE_URI}`,
   // timeout: 30000
@@ -11,19 +11,19 @@ async function responseValidate(error: any) {
   // API 서버 접속 오류
   if (!error.response || error.response.status === 404 || error.response.status === 504) {
     toast.error("API 서버 연결 오류");
-    return "api server connection error";
+    return !error.response ? 500 : error.response.status;
   }
 
   console.error(error.response);
 
   if (error.response.status === 401) {
-    const token = sessionStorage.getItem("token");
-    const res = await axios.post("/api/batch/retoken", null, {
+    const token: string | null = sessionStorage.getItem("token");
+    const res: AxiosResponse<any, any> = await axios.post("/api/batch/retoken", null, {
       headers: { Authorization: `Bearer ${token}` },
     });
 
-    if (res.data && res.data.token) {
-      sessionStorage.setItem("token", res.data.token);
+    if (res.data && res.data.newToken) {
+      sessionStorage.setItem("token", res.data.newToken);
     } else {
       toast.error("로그인이 만료되었습니다. 다시 로그인 해주세요.");
       sessionStorage.clear();
@@ -36,16 +36,16 @@ async function responseValidate(error: any) {
 
   if (error.response.data.message) {
     toast.error(error.response.data.message);
-
-    return error.response.data.message;
+  } else if (error.response.statusText) {
+    toast.error(error.response.statusText);
   }
 
   return error.response.status;
 }
 
 axiosUtils.interceptors.request.use(
-  (config) => {
-    const token = sessionStorage.getItem("token");
+  (config: InternalAxiosRequestConfig) => {
+    const token: string | null = sessionStorage.getItem("token");
 
     if (token && config.headers) {
       config.headers["Authorization"] = `Bearer ${token}`;
@@ -65,10 +65,10 @@ axiosUtils.interceptors.request.use(
 );
 
 axiosUtils.interceptors.response.use(
-  (response) => {
+  (response: AxiosResponse<any, any>) => {
     return response;
   },
-  (error) => {
+  (error: AxiosResponse<any, any>) => {
     return Promise.reject(responseValidate(error));
   }
 );
