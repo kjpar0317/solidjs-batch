fd009dd61e3893ae404285ffc9a7d810bb99442e
kjpar0317 <kjpar_kr@yahoo.co.kr>
2023-08-23 10:01:02 +0900

    Create LoadingSkeletonColumn.tsx

---
+++ frontend/src/components/grid/LoadingSkeletonColumn.tsx
@@ -0,0 +1,15 @@
+import { type JSXElement, Match, Switch } from "solid-js";
+import { ICellRendererParams } from "ag-grid-community";
+
+export default function LoadingSkeletonColumn(props: ICellRendererParams): JSXElement {
+  return (
+    <Switch>
+      <Match when={props.data}>{props.value}</Match>
+      <Match when={!props.data}>
+        <div class="w-full animate-pulse">
+          <p class="h-2 w-auto mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+        </div>
+      </Match>
+    </Switch>
+  );
+}
