0b2f1c35a88e05be81abf44eb32b722ad10d240e
kjpar0317 <kjpar_kr@yahoo.co.kr>
2023-10-18 14:07:48 +0900

    Create JwtTokenFilterConfigurer.java

---
+++ backend/test/JwtTokenFilterConfigurer.java
@@ -0,0 +1,20 @@
+package com.dw.ids.sonic.common.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenFilterConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@Override
+	public void configure(HttpSecurity http) {
+		JwtTokenFilter customFilter = new JwtTokenFilter(this.jwtTokenProvider);
+		http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+}
